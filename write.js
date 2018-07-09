9c16729604ceb38233cf9acedce792f9347983ce
Yamkovoi Alexei <yamkovoi.a@gmail.com>
2018-07-10 00:09:52 +0300

    write is work now

--- write.js
+++ write.js
@@ -68,7 +68,7 @@ const {Writable} = require('stream');
 
 const outStream = new Writable({
     write(chunk, encoding, callback){
-        console.log(chunk.toString());
+        console.log(chunk.toString().toUpperCase());
         callback()
     }
 })
